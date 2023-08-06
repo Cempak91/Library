158c1b5642c317511bddb05f4a9834dd5e2f7395
cempak91 <setrac91@gmail.com>
2023-08-06 21:06:22 +0400

    feat: add JS for burger

--- Library/index.js
+++ Library/index.js
@@ -1,20 +1,35 @@
 
 /*Меню бургера*/
-const navMenu = document.querySelector('.burger');
-const navSub = document.querySelector('.nav-sub');
-const navLink = document.querySelector('.navlink');
+const navMenu = document.querySelector(".burger");
+const navSub = document.querySelector(".nav-sub");
+const navLink = document.querySelectorAll(".navlink");
+const menuExit = document.querySelector(".burgerExit");
+
+console.log (navMenu);
+console.log(navSub);
 if (navMenu) {
     navMenu.addEventListener("click", function (e) {
-        navMenu.classList.toggle('_active');
-        navSub.classList.toggle('_active');
-        document.body.classList.toggle('lock');
+        navMenu.classList.toggle("_active");
+        navSub.classList.toggle("_active");
+        document.body.classList.toggle("lock");
+        menuExit.classList.toggle("open");
     });
 };
 
-if (navLink) {
-    navLink.addEventListener("click",function (e){
-        navMenu.classList.remove('_active');
-        navSub.classList.remove('_active');
-        document.body.classList.remove('lock');
+for (let i=0; i<navLink.length; i++) {
+    navLink[i].addEventListener("click",function (e){
+        navMenu.classList.remove("_active");
+        navSub.classList.remove("_active");
+        document.body.classList.remove("lock");
+        menuExit.classList.remove("open");
+});
+};
+
+if (menuExit) {
+    menuExit.addEventListener("click",function (e){
+        navMenu.classList.remove("_active");
+        navSub.classList.remove("_active");
+        document.body.classList.remove("lock");
+        menuExit.classList.remove("open");
     });
 };
